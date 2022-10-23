20c98965007f2bc86fffe3b8c83d10d47a9eb67d
Anton <antonsmit3045@gmail.com>
2022-10-23 16:50:12 +0200

    added additional documentation

--- main.py
+++ main.py
@@ -8,19 +8,28 @@ words_to_replace = ["Oracle", "Google", "Microsoft", "Amazon", "Deloitte"]
 
 @app.get("/")
 def home():
-    return {"message": "Success, API connection works. Perform a GET to /help to view methods"}
+    return {"message": "Success, API connection works. Perform a GET to /docs to get started."}
 
 @app.post("/replace")
 async def replace_string_words(request: Request):
-    """documentation"""
+    """
+Send a string that will replace the following keywords:\n
+"Oracle", "Google", "Microsoft", "Amazon", "Deloitte"\n
+with:\n
+"Oracle©", "Google©", "Microsoft©", "Amazon©", "Deloitte©"\n
+
+Method: POST\n
+Content-Type: application/json\n
+
+example:\n
+Request:\n
+{"data": "Google, Microsoft and Amazon are the top Cloud competitors."}\n
+
+Response:\n
+{"data": "Google©, Microsoft© and Amazon© are the top Cloud competitors."}\n
+    """
     data = await request.json()
     for i, w in enumerate(words_to_replace):
         if w in data['data']:
             data['data'] = data['data'].replace(w, w + special_char)
     return data
-
-@app.get("/help")
-def get_help():
-    return {
-        "message": ""
-        }
