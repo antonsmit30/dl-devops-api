9d7d13908d5bb8c7e0ff64576ebc74f47d3248df
Anton <antonsmit3045@gmail.com>
2022-10-27 16:41:21 +0200

    added documentation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,42 @@
 from fastapi import FastAPI, Request
 
-app = FastAPI()
+description = """
+## Overview
+Send a string that will replace the following keywords:\n
+```
+"Oracle", "Google", "Microsoft", "Amazon", "Deloitte"\n
+```
+with this character©:\n
+```
+"Oracle©", "Google©", "Microsoft©", "Amazon©", "Deloitte©"\n
+```
+## Data
+Method Type: **POST**\n
+Content-Type: **application/json**\n
+
+## Example:\n
+### Request Path:
+```
+/replace
+```
+
+### Request Body:\n
+```
+{ 
+    "data": "Google, Microsoft and Amazon are the top Cloud competitors."
+}\n
+```
+###Response:\n
+```
+{"data": "Google©, Microsoft© and Amazon© are the top Cloud competitors."
+}\n
+```
+"""
+
+app = FastAPI(
+    title="DlApi",
+    description=description
+)
 
 # code to use
 special_char = "©"
@@ -12,23 +48,6 @@ def home():
 
 @app.post("/replace")
 async def replace_string_words(request: Request):
-    """
-Send a string that will replace the following keywords:\n
-"Oracle", "Google", "Microsoft", "Amazon", "Deloitte"\n
-with this character©:\n
-"Oracle©", "Google©", "Microsoft©", "Amazon©", "Deloitte©"\n
-
-Method: POST\n
-Content-Type: application/json\n
-
-example:\n
-Request:\n
-{"data": "Google, Microsoft and Amazon are the top Cloud competitors."}\n
-
-Response:\n
-{"data": "Google©, Microsoft© and Amazon© are the top Cloud competitors."}\n
-
-    """
     data = await request.json()
     for i, w in enumerate(words_to_replace):
         if w in data['data']:
